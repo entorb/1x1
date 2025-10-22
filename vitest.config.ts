e895aba86302785f3c32663000e3fb8d01c60f26
Torben <59419684+entorb@users.noreply.github.com>
2025-10-22 02:54:36 +0200

    Vitest fixes after package udpate

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,40 +1,45 @@
 import { fileURLToPath } from 'node:url'
-import { defineConfig, configDefaults } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vite'
+import { configDefaults, defineConfig as defineVitestConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  test: {
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov', 'html'],
-      exclude: [
-        ...configDefaults.exclude,
-        'e2e/**',
-        'src/__tests__/**',
-        '**/*.spec.ts',
-        '**/*.test.ts'
-      ]
-    },
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/**', 'node_modules/'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-    setupFiles: ['./src/__tests__/setup.ts'],
-    css: {
-      modules: {
-        classNameStrategy: 'non-scoped'
-      }
-    },
-    server: {
-      deps: {
-        inline: ['vuetify']
+export default mergeConfig(
+  defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     }
-  }
-})
+  }),
+  defineVitestConfig({
+    test: {
+      globals: true,
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'lcov', 'html'],
+        exclude: [
+          ...configDefaults.exclude,
+          'e2e/**',
+          'src/__tests__/**',
+          '**/*.spec.ts',
+          '**/*.test.ts'
+        ]
+      },
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**', 'node_modules/'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: ['./src/__tests__/setup.ts'],
+      css: {
+        modules: {
+          classNameStrategy: 'non-scoped'
+        }
+      },
+      server: {
+        deps: {
+          inline: ['vuetify']
+        }
+      }
+    }
+  })
+)
